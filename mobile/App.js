3baa6b0f33a320567c33d14b9688fd1f63ac465a
Vladislav Manakov <vladislav.manakov@kelnik.ru>
2019-07-17 21:40:38 +0300

    #007 Пробует обновится до RN 0.6

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,23 @@
-import { Text } from 'react-native';
+import { Provider } from 'react-redux';
 import React from 'react';
+import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import Notification from 'components/Notification';
+import store from 'store';
+import Index from './pages/home';
+import About from './pages/about';
+
+const TabNavigator = createBottomTabNavigator({
+  Home: { screen: Index },
+  About: { screen: About }
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const App = () => (
-  <Text> Hello </Text>
+  <Provider store={store()}>
+    <AppContainer />
+    <Notification />
+  </Provider>
 );
 
 export default App;
